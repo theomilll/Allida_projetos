3adba025c7572ccf33c0ad076601396a29aaa710
TheoMoura <99195030+theomilll@users.noreply.github.com>
2022-11-13 20:52:09 -0300

    Add files via upload

--- Allida/settings.py
+++ Allida/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allida_app',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = ''
